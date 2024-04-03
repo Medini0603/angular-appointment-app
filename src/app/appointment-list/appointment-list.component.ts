16029037ecaf4c6f0e463d6dddef872076cc64ab
Medini <medu0603@gmail.com>
2024-04-03 16:48:11 +0530

    ngOnInit lifecycle hook to load appointments from local storage

--- src/app/appointment-list/appointment-list.component.ts
+++ src/app/appointment-list/appointment-list.component.ts
@@ -1,16 +1,26 @@
 import { Component } from '@angular/core';
 import { Appointment } from '../models/appointment';
-
+import { OnInit } from '@angular/core';
 @Component({
   selector: 'app-appointment-list',
   templateUrl: './appointment-list.component.html',
   styleUrls: ['./appointment-list.component.css']
 })
-export class AppointmentListComponent {
+export class AppointmentListComponent implements OnInit{
+  
   newAppointmentTitle:string="";
   newAppointmentDate:Date=new Date();
   appointments:Appointment[]=[]
 
+  // gets invoked when this component gets initialized 
+  ngOnInit(): void {
+    console.log("Got loaded")
+    let savedAppointments=localStorage.getItem('appointments')
+    //assign the appointments stored in local storage to the appointments arrays everytime we refresh page
+    // if appointments are already there parse it and load it into appointments array else initialize it to an empty array
+    // if we dont do this if we refresh page the appointments array becomes empty
+    this.appointments=savedAppointments?JSON.parse(savedAppointments):[]
+}
   addAppointment(){
     //check if both desc and date exists
     if(this.newAppointmentTitle.trim().length && this.newAppointmentDate){
